c4d08772ff4cb1299ada268df8e49e89480283c6
kienluu <kienluuharlow@gmail.com>
2012-05-02 15:14:39 +0100

    Added python tab complete and history plugin

--- custom/mycustom.zsh
+++ custom/mycustom.zsh
@@ -3,6 +3,23 @@ unsetopt correct_all
 ZSH_THEME_TERM_TAB_TITLE_IDLE="%m::%15<..<%~%<<"
 
 # Aliases
-alias ll='ls -lh'
-alias la='ls -lah'
+	alias ll='ls -lh'
+	alias la='ls -lah'
+
+	# Git
+	alias glp='gl;gp'
+
+	# Django Project
+	# Should find out how to use the $VIRTUAL_ENV variable to get this.  Because this will not work with the alias command.  It doesn't parse the $ token
+	alias migrate='python manage.py migrate'
+	alias schemamigration'python manage.py schemamigration'
+	alias runserver='python manage.py runserver'
+	alias collectstatic='python manage.py collectstatic'
+	alias mshell='python manage.py mshell'
+
+# Python tab complete and history plugin
+PYTHONSTARTUP=~/.oh-my-zsh/custom/pythonstartup.py
+export PYTHONSTARTUP
+
+
 
